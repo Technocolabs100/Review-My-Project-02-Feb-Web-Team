9289d5c9371df5bfe0452e23d325706c999abe7d
Sarikavidya <71083930+Sarikavidya@users.noreply.github.com>
2021-03-02 08:53:39 +0530

    Create registers.js
    added new file

---
+++ src/models/registers.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cpassword: {
+        type: String,
+        required: true
+    }
+})
+
+const Register = new mongoose.model("users", employeeSchema);
+
+module.exports = Register;
